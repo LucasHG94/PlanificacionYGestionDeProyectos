72e97a126528c81a17c131f00248faabd04832ff
lucashg94 <lucashg94@hotmail.es>
2016-01-03 21:43:14 +0100

    tras muchisimo esfuerzo creado el login que mantiene la sesion con cookies, reutilizar en las paginas todo el javascript

--- proyecto/GestionProyectos/src/java/servicioWeb/SimpleRootResource.java
+++ proyecto/GestionProyectos/src/java/servicioWeb/SimpleRootResource.java
@@ -5,18 +5,26 @@
  */
 package servicioWeb;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import modelo.Dedicacion;
 import modelo.Proyecto;
+import modelo.Trabajador;
+import persistencia.DedicacionFacadeLocal;
 import persistencia.ProyectoFacadeLocal;
+import persistencia.TrabajadorFacadeLocal;
 
 /**
  * REST Web Service
@@ -31,6 +39,12 @@ public class SimpleRootResource {
     
     @EJB
     private ProyectoFacadeLocal proyectoFacade;
+    
+    @EJB
+    private TrabajadorFacadeLocal trabajadorFacade;
+    
+    @EJB
+    private DedicacionFacadeLocal dedicacionFacade;
 
     /**
      * Creates a new instance of SimpleRootResource
@@ -61,10 +75,35 @@ public class SimpleRootResource {
     }
     
     @GET
-    @Produces("application/xml")
+    @Produces("application/json")
     @Path("/proyectos")
-    public List<Proyecto> getProyectos(){
+    public List<Proyecto> getProyectos(@QueryParam("user") String nombre){
         List<Proyecto> proyectos = proyectoFacade.findAll();
+        List<Dedicacion> dedicaciones = dedicacionFacade.findAll();
+        Trabajador t = trabajadorFacade.find(nombre);
+        List<Dedicacion> dedicFilt = new ArrayList<Dedicacion>();
+        for(Dedicacion item:dedicaciones){
+            if(item.getTrabajador().getNick().equals(t.getNick())){
+                dedicFilt.add(item);
+            }
+        }
+        
+        List<Proyecto> pFiltrado = new ArrayList<Proyecto>();
+        for(Dedicacion item:dedicFilt){
+            pFiltrado.add(item.getProyecto());
+        }
         return proyectos;
+    }
+    
+    @GET
+    @Path("/usuario")
+    public boolean login(@QueryParam("user") String nombre, @QueryParam("password") String contraseña){
+           List<Trabajador> trabajadores = trabajadorFacade.findAll();
+           for(Trabajador item : trabajadores){              
+               if(item.getNick().equals(nombre)){
+                   return item.getPassword().compareTo(contraseña)==0;
+               }
+           }
+           return false;
     }
 }
