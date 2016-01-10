ecc19b87cbd4c6837e871b77a22a597bdbb72318
Ironn94 <roberto_94_va@hotmail.com>
2016-01-10 22:25:08 +0100

    cu alta de usuario

---
+++ proyecto/GestionProyectos/web/js/altaUsuario.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+checkAdmin();
+
+jQuery("#cancelBtn").click(function () {
+        var pagina = 'http://localhost:8080/GestionProyectos/admin.html';
+        document.location.href = pagina;
+});
+
+jQuery("#confirmBtn").click(function () {
+    var nick = $("#nick").val();
+    var pass = $("#password").val();
+    var pass2 = $("#password").val();
+    var cat = $("#categoria").val();
+    if(pass != pass2){
+        window.alert("Las contraseñas no coinciden");
+        var pagina = 'http://localhost:8080/GestionProyectos/alta_Usuario.html';
+        document.location.href = pagina;
+    }
+    if(cat<1 && cat>4){
+        window.alert("La categoria debe estar entre 1 y 4");
+        var pagina = 'http://localhost:8080/GestionProyectos/alta_Usuario.html';
+        document.location.href = pagina;
+    }
+    jQuery.get("http://localhost:8080/GestionProyectos/webresources/SimpleRoot/admin/newUser", {
+        nick: nick, pass:pass, cat:cat
+    }, function () {      
+        var pagina = 'http://localhost:8080/GestionProyectos/admin.html';
+        document.location.href = pagina;
+    });
+
+
+});
+
